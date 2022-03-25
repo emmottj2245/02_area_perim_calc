65a7177f2320ecee7accb72dcc55459ba302813f
emmottj2245 <101373326+emmottj2245@users.noreply.github.com>
2022-03-25 14:48:55 +1300

    Add files via upload

---
+++ 01_looping_basic.py
@@ -0,0 +1,14 @@
+valid = False
+while not valid:
+
+    response = float(inout("enter a number:"))
+
+if response > 0:
+    valid = true
+
+else:
+    print("please enter a number that is more than zero")
+    print() 
+
+
+    
