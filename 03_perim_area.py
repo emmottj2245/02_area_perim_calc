4860a193e32e6ab8b8a67d9bc7c498cab08a26e1
emmottj2245 <101373326+emmottj2245@users.noreply.github.com>
2022-04-04 13:13:04 +1200

    Add files via upload
    adding permieter and other stuff

---
+++ 03_perim_area.py
@@ -0,0 +1,27 @@
+def num_check (question)
+    valid = False
+    while not valid:
+
+        error = "please enter a number that is more than zero"
+
+        try:
+
+                response = float(input(question))
+
+            if response > 0:
+                return response
+
+            else:
+                print("please enter a number that is more than zero")
+                print() 
+
+        except ValueError:
+            print(error)
+    
+
+
+    width = num_check ("width: ")
+    height = num_check("height")
+  area = width * height
+  print("perimeter: {} units".format(perimeter))
+print("area: {} sqaure units".format(area))
