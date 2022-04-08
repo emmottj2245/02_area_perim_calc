4d00d1827b8a4c4363ef2e2a67c1d67f8970856a
emmottj2245 <101373326+emmottj2245@users.noreply.github.com>
2022-04-08 14:51:58 +1200

    Add files via upload

--- 04_perim_area_calculator_v2.py
+++ 04_perim_area_calculator_v2.py
@@ -1,3 +1,6 @@
+#functions go here
+
+# check input is a number more than zero
 def num_check (question):
     valid = False
     while not valid:
@@ -5,27 +8,48 @@ def num_check (question):
         error = "please enter a valid number"
 
         try:
-
-                response = float(input(question))
-
-                if response > 0:
-                    return response
-
-                else:
-                    print("please enter a number that is more than zero")
-                    print() 
+            
+            #ask user to enter a number
+            response = float(input(question))
+            
+            # check number is more than zero
+            if response > 0:
+                return response
+            
+            #outputs error if input is invalid
+            else:
+                print("please enter a number that is more than zero")
+                print() 
 
         except ValueError:
             print(error)
 
+#main routine goes here
 
+# introduction?heading prin statements
+print()
+print("**** area perimeter calculator****")
+print()
 
-width = num_check ("width: ")
-height = num_check("height: ")
-area = width * height
-perimeter = 2 * (width + height)
-print("perimeter: {} units".format(perimeter))
-print("area: {} sqaure units".format(area))
+# start of the calculator loop
+keep_going = ""
+while keep_going == "":
+
+    width = num_check ("width: ")
+    height = num_check("height: ")
+    
+    # calculate area (width x height)
+    area = width * height
+    perimeter = 2 * (width + height)
+
+    #output area and perimeter to 2dp
+    print("perimeter: {} units".format(perimeter))
+    print("area: {} sqaure units".format(area))
+
+    keep_going = input("press <enter> to keep going or any key to quit")
+
+print()
+print("thanks fo using the area/perimeter calculator")
 
 
     
